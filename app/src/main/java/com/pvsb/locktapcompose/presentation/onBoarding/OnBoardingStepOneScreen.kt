79f7ac511bb9d95d51c9c948e45b8a5a6e5f7d9e
borges <paulo.borges@premiersoft.net>
2023-03-23 22:07:20 -0200

    feat: add bottom sheet for on boarding screen

--- app/src/main/java/com/pvsb/locktapcompose/presentation/onBoarding/OnBoardingStepOneScreen.kt
+++ app/src/main/java/com/pvsb/locktapcompose/presentation/onBoarding/OnBoardingStepOneScreen.kt
@@ -1,18 +1,146 @@
 package com.pvsb.locktapcompose.presentation.onBoarding
 
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.BottomSheetScaffold
+import androidx.compose.material.BottomSheetValue
+import androidx.compose.material.Button
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetState
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.rememberBottomSheetScaffoldState
+import androidx.compose.material.rememberBottomSheetState
+import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.pvsb.locktapcompose.R
 import com.pvsb.locktapcompose.presentation.onBoarding.shared.ComposeOnBoardingPrintsBackgroundBuilder
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun OnBoardingStepOneScreen() {
 
-    val drawables = listOf(
-        R.drawable.on_boarding_step_1_1,
-        R.drawable.on_boarding_step_1_2,
-        R.drawable.on_boarding_step_1_3,
-        R.drawable.on_boarding_step_1_4,
+    Box(
+        contentAlignment = Alignment.BottomCenter, modifier = Modifier.fillMaxSize()
+    ) {
+
+        val drawables = listOf(
+            R.drawable.on_boarding_step_1_1,
+            R.drawable.on_boarding_step_1_2,
+            R.drawable.on_boarding_step_1_3,
+            R.drawable.on_boarding_step_1_4,
+        )
+
+        val modalSheetState = rememberModalBottomSheetState(
+            initialValue = ModalBottomSheetValue.Expanded,
+            confirmStateChange = { false },
+            skipHalfExpanded = true
+        )
+
+        ComposeOnBoardingPrintsBackgroundBuilder(drawables)
+
+        ModalBottomSheetLayout(
+            modifier = Modifier
+                .fillMaxHeight(0.4f)
+                .fillMaxWidth(),
+            sheetContent = {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(colorResource(id = R.color.bg_secondary))
+                ) {
+                    Text(text = "This is a bottom sheet", fontSize = 30.sp)
+                }
+            },
+            sheetState = modalSheetState,
+            sheetShape = RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp),
+            scrimColor = Color.Transparent
+        ) {}
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun Testeee() {
+
+    val coroutineScope = rememberCoroutineScope()
+    val modalSheetState = rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden,
+        confirmStateChange = { it != ModalBottomSheetValue.HalfExpanded },
+        skipHalfExpanded = true
     )
 
-    ComposeOnBoardingPrintsBackgroundBuilder(drawables)
+    var isSheetFullScreen by remember { mutableStateOf(false) }
+    val roundedCornerRadius = if (isSheetFullScreen) 0.dp else 12.dp
+    val modifier = if (isSheetFullScreen) Modifier.fillMaxSize()
+    else Modifier.fillMaxWidth()
+
+    BackHandler(modalSheetState.isVisible) {
+        coroutineScope.launch { modalSheetState.hide() }
+    }
+
+    ModalBottomSheetLayout(sheetState = modalSheetState, sheetShape = RoundedCornerShape(
+        topStart = roundedCornerRadius, topEnd = roundedCornerRadius
+    ), sheetContent = {
+        Column(
+            modifier = modifier,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Button(onClick = {
+                isSheetFullScreen = !isSheetFullScreen
+            }) {
+                Text(text = "Toggle Sheet Fullscreen")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch { modalSheetState.hide() }
+            }) {
+                Text(text = "Hide Sheet")
+            }
+        }
+    }) {
+        Scaffold {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                Button(
+                    onClick = {
+                        coroutineScope.launch {
+                            if (modalSheetState.isVisible) modalSheetState.hide()
+                            else modalSheetState.animateTo(ModalBottomSheetValue.Expanded)
+                        }
+                    },
+                ) {
+                    Text(text = "Open Sheet")
+                }
+            }
+
+            it
+        }
+    }
 }
