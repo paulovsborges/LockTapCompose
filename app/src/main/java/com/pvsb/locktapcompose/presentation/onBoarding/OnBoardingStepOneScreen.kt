68f86a49785d2b1fc63931a3bb6f91c5348a1925
borges <paulo.borges@premiersoft.net>
2023-03-24 19:57:10 -0200

    feat: add content of the first step on bottom sheet

--- app/src/main/java/com/pvsb/locktapcompose/presentation/onBoarding/OnBoardingStepOneScreen.kt
+++ app/src/main/java/com/pvsb/locktapcompose/presentation/onBoarding/OnBoardingStepOneScreen.kt
@@ -1,43 +1,47 @@
 package com.pvsb.locktapcompose.presentation.onBoarding
 
-import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.BottomSheetScaffold
-import androidx.compose.material.BottomSheetValue
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonColors
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetLayout
-import androidx.compose.material.ModalBottomSheetState
 import androidx.compose.material.ModalBottomSheetValue
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.rememberBottomSheetScaffoldState
-import androidx.compose.material.rememberBottomSheetState
 import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.pvsb.locktapcompose.R
 import com.pvsb.locktapcompose.presentation.onBoarding.shared.ComposeOnBoardingPrintsBackgroundBuilder
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -67,13 +71,11 @@ fun OnBoardingStepOneScreen() {
                 .fillMaxHeight(0.4f)
                 .fillMaxWidth(),
             sheetContent = {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(colorResource(id = R.color.bg_secondary))
-                ) {
-                    Text(text = "This is a bottom sheet", fontSize = 30.sp)
-                }
+                BottomSheetContent(
+                    stringResource(id = R.string.on_boarding_step_1_title),
+                    stringResource(id = R.string.on_boarding_step_1_message),
+                    modifier = Modifier.background(colorResource(id = R.color.bg_secondary))
+                )
             },
             sheetState = modalSheetState,
             sheetShape = RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp),
@@ -82,65 +84,132 @@ fun OnBoardingStepOneScreen() {
     }
 }
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun Testeee() {
+private fun BottomSheetContent(
+    title: String,
+    message: String,
+    modifier: Modifier = Modifier
+) {
 
-    val coroutineScope = rememberCoroutineScope()
-    val modalSheetState = rememberModalBottomSheetState(
-        initialValue = ModalBottomSheetValue.Hidden,
-        confirmStateChange = { it != ModalBottomSheetValue.HalfExpanded },
-        skipHalfExpanded = true
-    )
+    var selectedPage by remember { mutableStateOf(1) }
 
-    var isSheetFullScreen by remember { mutableStateOf(false) }
-    val roundedCornerRadius = if (isSheetFullScreen) 0.dp else 12.dp
-    val modifier = if (isSheetFullScreen) Modifier.fillMaxSize()
-    else Modifier.fillMaxWidth()
+    Box(modifier = modifier.fillMaxSize()) {
 
-    BackHandler(modalSheetState.isVisible) {
-        coroutineScope.launch { modalSheetState.hide() }
-    }
-
-    ModalBottomSheetLayout(sheetState = modalSheetState, sheetShape = RoundedCornerShape(
-        topStart = roundedCornerRadius, topEnd = roundedCornerRadius
-    ), sheetContent = {
         Column(
-            modifier = modifier,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Button(onClick = {
-                isSheetFullScreen = !isSheetFullScreen
-            }) {
-                Text(text = "Toggle Sheet Fullscreen")
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(32.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                PageIndicator(currentPage = selectedPage)
             }
 
-            Button(onClick = {
-                coroutineScope.launch { modalSheetState.hide() }
-            }) {
-                Text(text = "Hide Sheet")
+            Spacer(modifier = Modifier.height(25.dp))
+
+            Text(
+                text = title,
+                fontFamily = FontFamily(
+                    Font(
+                        R.font.sf_pro_display_medium,
+                        weight = FontWeight.SemiBold
+                    )
+                ),
+                color = Color.White,
+                fontSize = 24.sp
+            )
+
+            Text(
+                text = message,
+                fontFamily = FontFamily(
+                    Font(
+                        R.font.sf_pro_display_regular,
+                        weight = FontWeight.Thin
+                    )
+                ),
+                color = colorResource(id = R.color.gray),
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(horizontal = 38.dp, vertical = 12.dp)
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Button(
+                onClick = {
+                    selectedPage++
+                },
+                shape = RoundedCornerShape(corner = CornerSize(40.dp)),
+                modifier = Modifier
+                    .width(335.dp)
+                    .height(52.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = colorResource(id = R.color.light_blue)
+                )
+            ) {
+                Text(
+                    text = stringResource(id = R.string.button_label_next),
+                    fontFamily = FontFamily(
+                        Font(
+                            R.font.sf_pro_display_regular,
+                            weight = FontWeight.Thin
+                        )
+                    ),
+                    color = Color.White
+                )
             }
         }
-    }) {
-        Scaffold {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center,
-            ) {
-                Button(
-                    onClick = {
-                        coroutineScope.launch {
-                            if (modalSheetState.isVisible) modalSheetState.hide()
-                            else modalSheetState.animateTo(ModalBottomSheetValue.Expanded)
-                        }
-                    },
-                ) {
-                    Text(text = "Open Sheet")
-                }
-            }
+    }
+}
 
-            it
+@Preview
+@Composable
+fun BottomSheetContentPreview() {
+    BottomSheetContent(
+        stringResource(id = R.string.on_boarding_step_1_title),
+        stringResource(id = R.string.on_boarding_step_1_message)
+    )
+}
+
+@Composable
+fun PageIndicator(
+    modifier: Modifier = Modifier,
+    totalPages: Int = 3,
+    currentPage: Int = 2,
+) {
+
+    val notSelectedColor = Color(0, 0, 0, 50)
+    val selectedColor = Color.White
+
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+
+        for (page in 1..totalPages) {
+
+            val backgroundColor = if (page == currentPage) {
+                selectedColor
+            } else notSelectedColor
+
+            Card(
+                modifier = modifier
+                    .height(4.dp)
+                    .width(24.dp),
+                shape = RoundedCornerShape(5f),
+                backgroundColor = backgroundColor,
+                contentColor = backgroundColor
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(backgroundColor)
+                )
+            }
         }
     }
 }
