26058f4279a755a8b5a8df19d03458ae85a08cd1
borges <paulo.borges@premiersoft.net>
2023-03-31 20:21:13 -0200

    feat: session options buttons

--- app/src/main/java/com/pvsb/locktapcompose/presentation/main/shared/searchFIeld/ComposeSearchField.kt
+++ app/src/main/java/com/pvsb/locktapcompose/presentation/main/shared/searchFIeld/ComposeSearchField.kt
@@ -1,5 +1,6 @@
 package com.pvsb.locktapcompose.presentation.main.shared.searchFIeld
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -15,8 +16,13 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,6 +30,7 @@ import androidx.compose.ui.unit.dp
 import com.pvsb.locktapcompose.R
 import com.pvsb.locktapcompose.presentation.ui.theme.AppColors
 import com.pvsb.locktapcompose.presentation.ui.theme.AppColors.gray
+import com.pvsb.locktapcompose.presentation.ui.theme.AppColors.lightBlue
 
 @Composable
 fun ComposeSearchField(
@@ -35,9 +42,15 @@ fun ComposeSearchField(
     val placeHolderText = stringResource(id = R.string.search_field_placeholder_enter_a_title)
     val textToDisplay = text.ifEmpty { placeHolderText }
     val textColor = if (text.isEmpty()) gray else Color.White
+    var isTextFieldFocused by remember { mutableStateOf(false) }
+    val borderColor = if (isTextFieldFocused) lightBlue else Color.Transparent
 
     BasicTextField(
-        modifier = modifier.height(44.dp),
+        modifier = modifier
+            .height(44.dp)
+            .onFocusChanged {
+                isTextFieldFocused = it.isFocused
+            },
         value = text,
         onValueChange = onValueChanged,
         decorationBox = {
@@ -46,12 +59,15 @@ fun ComposeSearchField(
                 modifier = Modifier
                     .fillMaxSize(),
                 color = AppColors.secondary,
-                shape = RoundedCornerShape(5.dp)
+                shape = RoundedCornerShape(5.dp),
+                border = BorderStroke(1.dp, color = borderColor)
             ) {
 
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.fillMaxSize().padding(start = 8.dp),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(start = 8.dp),
                     horizontalArrangement = Arrangement.spacedBy(5.dp)
                 ) {
                     Icon(
@@ -63,7 +79,7 @@ fun ComposeSearchField(
                     Text(text = textToDisplay, color = textColor)
                 }
             }
-        }
+        },
     )
 }
 
