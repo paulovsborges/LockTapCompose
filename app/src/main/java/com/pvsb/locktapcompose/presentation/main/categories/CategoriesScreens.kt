38dd04a88a464d7707f176bedd21d1d22ba78a94
borges <paulo.borges@premiersoft.net>
2023-03-31 20:32:43 -0200

    feat: add navigation for private contacts activity

--- app/src/main/java/com/pvsb/locktapcompose/presentation/main/categories/CategoriesScreens.kt
+++
@@ -1,9 +0,0 @@
-package com.pvsb.locktapcompose.presentation.main.categories
-
-sealed class CategoriesScreens(
-    val route: String
-){
-
-    object SessionOptions: CategoriesScreens("main_categories_session_options")
-
-}
---
+++ app/src/main/java/com/pvsb/locktapcompose/presentation/main/categories/privateContacts/PrivateContactsActivity.kt
@@ -0,0 +1,25 @@
+package com.pvsb.locktapcompose.presentation.main.categories.privateContacts
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+class PrivateContactsActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "private contacts")
+            }
+        }
+    }
+}
