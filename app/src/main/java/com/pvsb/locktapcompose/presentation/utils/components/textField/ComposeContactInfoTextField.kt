4a259bb9cdaefe048f347795bb31e54c8336cacd
borges <paulo.borges@premiersoft.net>
2023-04-01 19:09:38 -0200

    fix: cursor position on contact info text field

--- app/src/main/java/com/pvsb/locktapcompose/presentation/utils/components/textField/ComposeContactInfoTextField.kt
+++ app/src/main/java/com/pvsb/locktapcompose/presentation/utils/components/textField/ComposeContactInfoTextField.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -18,13 +20,19 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,6 +40,7 @@ import androidx.compose.ui.unit.sp
 import com.pvsb.locktapcompose.R
 import com.pvsb.locktapcompose.presentation.ui.theme.AppColors
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun ComposeContactInfoTextField(
     modifier: Modifier = Modifier,
@@ -39,6 +48,9 @@ fun ComposeContactInfoTextField(
     fieldLabel: Int = 0,
     onValueChanged: (String) -> Unit = {}
 ) {
+
+    val focusManager = LocalFocusManager.current
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     val placeHolderText = stringResource(id = R.string.search_field_placeholder_enter_a_title)
     val textToDisplay = text.ifEmpty { placeHolderText }
@@ -53,17 +65,7 @@ fun ComposeContactInfoTextField(
                 isTextFieldFocused = it.isFocused
             },
         value = text,
-        onValueChange = {
-
-            val t = TextFieldValue(
-                text, TextRange(
-                    it.length
-                )
-            )
-            t.text
-
-            onValueChanged(it)
-        },
+        onValueChange = onValueChanged,
         decorationBox = {
 
             Surface(
@@ -91,10 +93,21 @@ fun ComposeContactInfoTextField(
                         fontSize = 10.sp
                     )
 
-                    Text(text = textToDisplay, color = textColor, fontSize = 14.sp)
+                    Box(modifier = Modifier.fillMaxWidth()) {
+                        it()
+                        Text(text = textToDisplay, color = textColor, fontSize = 14.sp)
+                    }
                 }
             }
-        }, cursorBrush = SolidColor(Color.White)
+        }, cursorBrush = SolidColor(Color.White),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text,
+            imeAction = ImeAction.Done
+        ),
+        keyboardActions = KeyboardActions(onDone = {
+            focusManager.clearFocus()
+            keyboardController?.hide()
+        })
     )
 }
 
