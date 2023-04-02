39de290fbde030523490aebdf6974d8a62f61f72
borges <paulo.borges@premiersoft.net>
2023-04-02 19:53:10 -0200

    feat: add modules declaration on app

--- datasource/build.gradle.kts
+++ datasource/build.gradle.kts
@@ -35,10 +35,12 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(project(Modules.domain))
+
+    implementation(AndroidX.core)
+    implementation(AndroidX.lifeCycle)
+    implementation(AndroidX.appCompat)
+    testImplementation(Test.jUnit)
+    androidTestImplementation(Test.jUInitExt)
+    androidTestImplementation(Test.espresso)
 }
--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -35,6 +35,8 @@ android {
 
 dependencies {
 
+    implementation(project(Modules.domain))
+
     implementation(AndroidX.core)
     implementation(AndroidX.lifeCycle)
     implementation(AndroidX.appCompat)
