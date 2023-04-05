9cad797d6288a2357840300ddbca1d46d0db4f77
borges <paulo.borges@premiersoft.net>
2023-04-05 21:37:08 -0200

    fix: hilt dependency

--- buildSrc/src/main/java/Hilt.kt
+++ buildSrc/src/main/java/Hilt.kt
@@ -1,5 +1,5 @@
 object Hilt {
-    const val hiltVersion = "2.42"
+    const val hiltVersion = "2.45"
     val android = "com.google.dagger:hilt-android:$hiltVersion"
     val compiler = "com.google.dagger:hilt-compiler:$hiltVersion"
     val compose = "androidx.hilt:hilt-navigation-compose:1.0.0"
