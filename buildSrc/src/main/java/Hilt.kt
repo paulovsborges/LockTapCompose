fc2380e12e9b396d401d648ccf27bc4aa569d50d
borges <paulo.borges@premiersoft.net>
2023-04-05 19:13:30 -0200

    feat: add dependency for sql delight

--- buildSrc/src/main/java/Hilt.kt
+++ buildSrc/src/main/java/Hilt.kt
@@ -1,5 +1,6 @@
 object Hilt {
-    const val hiltVersion = "2.41"
-    const val android = "com.google.dagger:hilt-android:$hiltVersion"
-    const val compiler = "com.google.dagger:hilt-compiler:$hiltVersion"
+    private const val hiltVersion = "2.42"
+    val android = "com.google.dagger:hilt-android:$hiltVersion"
+    val compiler = "com.google.dagger:hilt-compiler:$hiltVersion"
+    val compose = "androidx.hilt:hilt-navigation-compose:1.0.0"
 }
---
+++ buildSrc/src/main/java/SqlDelight.kt
@@ -0,0 +1,6 @@
+object SqlDelight {
+    private val version = "1.5.2"
+    val driver = "com.squareup.sqldelight:android-driver:$version"
+    val jvm = "com.squareup.sqldelight:coroutines-extensions-jvm:$version"
+    val plugin = "com.squareup.sqldelight:gradle-plugin:$version"
+}
