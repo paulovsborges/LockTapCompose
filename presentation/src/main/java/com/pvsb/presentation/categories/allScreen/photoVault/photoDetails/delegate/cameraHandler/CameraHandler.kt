770e516a4310485d0f9cf1e9682fbc76f6b79b36
borges <paulo.borges@premiersoft.net>
2023-04-23 14:35:20 -0200

    refactor: remove unused code

--- presentation/src/main/java/com/pvsb/presentation/categories/allScreen/photoVault/photoDetails/delegate/cameraHandler/CameraHandler.kt
+++ presentation/src/main/java/com/pvsb/presentation/categories/allScreen/photoVault/photoDetails/delegate/cameraHandler/CameraHandler.kt
@@ -1,21 +1,15 @@
 package com.pvsb.presentation.categories.allScreen.photoVault.photoDetails.delegate.cameraHandler
 
-import android.content.ContentValues
 import android.graphics.Bitmap
-import android.os.Build
-import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 class CameraHandler : ICameraHandler {
 
-    private var imageCapture: ImageCapture? = null
     private var isLensFacingBack: Boolean = true
     private var previewView: PreviewView? = null
 
@@ -44,7 +38,7 @@ class CameraHandler : ICameraHandler {
                     }
                 } ?: return@addListener
 
-                imageCapture = ImageCapture.Builder().build()
+                val imageCapture = ImageCapture.Builder().build()
 
                 try {
                     cameraProvider.unbindAll()
@@ -56,56 +50,6 @@ class CameraHandler : ICameraHandler {
         )
     }
 
-    override fun ComponentActivity.takePhoto(onPhotoSaved: (Bitmap) -> Unit) {
-        val imageCapture = imageCapture ?: return
-
-        val name = SimpleDateFormat(
-            FILENAME_FORMAT, Locale.ENGLISH
-        ).format(System.currentTimeMillis())
-
-        val contentValues = ContentValues().apply {
-            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
-            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
-            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
-            }
-        }
-
-        val outputOptions = ImageCapture.OutputFileOptions.Builder(
-            contentResolver,
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-            contentValues
-        ).build()
-
-//        imageCapture.takePicture(
-//            ContextCompat.getMainExecutor(this),
-//            @ExperimentalGetImage object : ImageCapture.OnImageCapturedCallback() {
-//                override fun onCaptureSuccess(proxy: ImageProxy) {
-////                    val bitMap = BitmapUtils.getBitmap(proxy)
-////
-////                    val bitMap = proxy.image?.toBitmap()
-////                    bitMap?.let(onPhotoSaved)
-//
-//
-//
-//
-//                    proxy.close()
-//                }
-//            })
-
-//        imageCapture.takePicture(outputOptions,
-//            ContextCompat.getMainExecutor(this),
-//            object : ImageCapture.OnImageSavedCallback {
-//                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-//                    outputFileResults.savedUri?.let(onPhotoSaved)
-//                }
-//
-//                override fun onError(exception: ImageCaptureException) {
-//                    exception.printStackTrace()
-//                }
-//            })
-    }
-
     override fun ComponentActivity.toggleLensFacing(
         previewView: PreviewView
     ) {
@@ -115,9 +59,5 @@ class CameraHandler : ICameraHandler {
 
     override fun getBitMapFromPreview(): Bitmap? {
         return previewView?.bitmap
-    }
-
-    private companion object {
-        const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
     }
 }
--- presentation/src/main/java/com/pvsb/presentation/categories/allScreen/photoVault/photoDetails/delegate/cameraHandler/ICameraHandler.kt
+++ presentation/src/main/java/com/pvsb/presentation/categories/allScreen/photoVault/photoDetails/delegate/cameraHandler/ICameraHandler.kt
@@ -7,7 +7,6 @@ import androidx.camera.view.PreviewView
 
 interface ICameraHandler {
     fun ComponentActivity.startCamera(previewView: PreviewView)
-    fun ComponentActivity.takePhoto(onPhotoSaved: (Bitmap) -> Unit)
     fun ComponentActivity.toggleLensFacing(previewView: PreviewView)
     fun getBitMapFromPreview(): Bitmap?
 }
