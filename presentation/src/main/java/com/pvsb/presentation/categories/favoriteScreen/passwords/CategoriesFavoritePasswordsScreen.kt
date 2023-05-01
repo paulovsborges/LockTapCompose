1a8bc58d058d27bd2739fb3f4d135b3c723f1f34
borges <paulo.borges@premiersoft.net>
2023-05-01 11:16:27 -0200

    refactor: move password card to its own component

---
+++ presentation/src/main/java/com/pvsb/presentation/categories/favoriteScreen/passwords/CategoriesFavoritePasswordsScreen.kt
@@ -0,0 +1,18 @@
+package com.pvsb.presentation.categories.favoriteScreen.passwords
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CategoriesFavoritePasswordsScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@Preview
+@Composable
+fun CategoriesFavoritePasswordsScreenPreview() {
+    CategoriesFavoritePasswordsScreen()
+}
--- presentation/src/main/java/com/pvsb/presentation/passwords/passwordsList/PasswordsListActivity.kt
+++ presentation/src/main/java/com/pvsb/presentation/passwords/passwordsList/PasswordsListActivity.kt
@@ -7,38 +7,22 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Favorite
-import androidx.compose.material.icons.rounded.FavoriteBorder
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -53,7 +37,6 @@ import com.pvsb.presentation.utils.components.BackButton
 import com.pvsb.presentation.utils.components.FloatingAddButton
 import com.pvsb.presentation.utils.components.textField.ComposePrimarySearchField
 import com.pvsb.presentation.utils.copyTextToClipBoard
-import com.pvsb.presentation.utils.formatToStringDate
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -132,8 +115,7 @@ class PasswordsListActivity : ComponentActivity() {
 
     @Composable
     private fun ComposePasswordsList(
-        modifier: Modifier = Modifier,
-        passwords: List<Password>
+        modifier: Modifier = Modifier, passwords: List<Password>
     ) {
 
         Box(modifier = modifier.fillMaxSize()) {
@@ -141,7 +123,13 @@ class PasswordsListActivity : ComponentActivity() {
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
                 items(passwords) {
-                    ComposePasswordCard(password = it)
+                    ComposePasswordCard(password = it, onCardClick = { passwordId ->
+                        navigateToDetails(passwordId)
+                    }, onCopyPassword = { password ->
+                        copyTextToClipBoard(password)
+                    }, onFavoriteClick = { passwordId ->
+                        viewModel.toggleFavorite(passwordId)
+                    })
                 }
             }
         }
@@ -182,150 +170,6 @@ class PasswordsListActivity : ComponentActivity() {
         }
     }
 
-    @Composable
-    private fun ComposePasswordCard(
-        modifier: Modifier = Modifier,
-        password: Password
-    ) {
-
-        Card(
-            modifier = modifier
-                .fillMaxWidth()
-                .wrapContentHeight(unbounded = true)
-                .clickable {
-                    navigateToDetails(password.id)
-                },
-            shape = RoundedCornerShape(8.dp), backgroundColor = AppColors.secondary
-        ) {
-            Box {
-
-                Column(
-                    modifier = Modifier.padding(top = 12.dp, start = 15.dp)
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.password_card_title_label),
-                        color = AppColors.gray,
-                        fontFamily = FontFamily(Font(R.font.sf_pro_display_regular)),
-                        fontSize = 10.sp
-                    )
-
-                    Spacer(modifier = Modifier.height(5.dp))
-
-                    Text(
-                        text = password.title,
-                        color = Color.White,
-                        fontFamily = FontFamily(Font(R.font.sf_pro_display_regular)),
-                        fontSize = 14.sp
-                    )
-
-                    Spacer(modifier = Modifier.height(12.dp))
-
-                    Text(
-                        text = stringResource(id = R.string.password_card_password_label),
-                        color = AppColors.gray,
-                        fontFamily = FontFamily(Font(R.font.sf_pro_display_regular)),
-                        fontSize = 10.sp
-                    )
-
-                    Spacer(modifier = Modifier.height(5.dp))
-
-                    Text(
-                        text = password.password,
-                        color = Color.White,
-                        fontFamily = FontFamily(Font(R.font.sf_pro_display_regular)),
-                        fontSize = 14.sp
-                    )
-
-                    Spacer(modifier = Modifier.height(25.dp))
-
-                    Box(
-                        contentAlignment = Alignment.CenterEnd,
-                        modifier = Modifier.padding(end = 16.dp)
-                    ) {
-
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .clickable {
-                                    copyTextToClipBoard(password.password)
-                                }
-                        ) {
-
-                            Icon(
-                                painter = painterResource(id = R.drawable.ic_copy),
-                                contentDescription = "",
-                                tint = AppColors.gray
-                            )
-
-                            Spacer(modifier = Modifier.width(5.dp))
-
-                            Text(
-                                text = stringResource(id = R.string.copy_password),
-                                fontSize = 16.sp,
-                                fontFamily = FontFamily(Font(R.font.sf_pro_display_regular)),
-                                color = AppColors.gray
-                            )
-                        }
-
-                        Text(
-                            text = password.createdAt?.formatToStringDate() ?: "",
-                            fontFamily = FontFamily(Font(R.font.sf_pro_display_regular)),
-                            color = AppColors.gray,
-                            fontSize = 14.sp,
-                            modifier = Modifier.alpha(0.6f)
-                        )
-                    }
-
-                    Spacer(modifier = Modifier.height(15.dp))
-                }
-
-                Box(modifier = Modifier.fillMaxSize()) {
-                    ComposeFavoriteButton(
-                        isFavorite = password.isFavorite
-                    ) {
-                        viewModel.toggleFavorite(password.id)
-                    }
-                }
-            }
-        }
-    }
-
-    @Composable
-    private fun ComposeFavoriteButton(
-        modifier: Modifier = Modifier,
-        isFavorite: Boolean = false,
-        onFavoriteClicked: (Boolean) -> Unit = {}
-    ) {
-        Row(
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(vertical = 25.dp, horizontal = 28.dp),
-            horizontalArrangement = Arrangement.End
-        ) {
-            Box(
-                modifier = Modifier.clickable {
-                    onFavoriteClicked(!isFavorite)
-                }
-            ) {
-                if (isFavorite) {
-
-                    Icon(
-                        modifier = Modifier.size(25.dp),
-                        imageVector = Icons.Rounded.Favorite,
-                        contentDescription = "",
-                        tint = AppColors.lightBlue
-                    )
-                } else {
-                    Icon(
-                        imageVector = Icons.Rounded.FavoriteBorder,
-                        contentDescription = "",
-                        tint = Color.White
-                    )
-                }
-            }
-        }
-    }
-
     private fun navigateToDetails(
         passwordId: String? = null
     ) {
@@ -339,21 +183,23 @@ class PasswordsListActivity : ComponentActivity() {
         startActivity(intent)
     }
 
-    @Preview(showBackground = true)
-    @Composable
-    private fun ComposePasswordCardPreview() {
-        Box(modifier = Modifier.padding(20.dp)) {
-            ComposePasswordCard(
-                password = Password(
-                    "", "Home wifi", "123456", null, true, null
-                )
-            )
-        }
-    }
-
     @Preview
     @Composable
     private fun ComposeContentPreview() {
-        ComposeContent()
+        ComposeContent(
+            PasswordsListState(
+                passwords = listOf(
+                    Password(
+                        "", "Home wifi", "123456", null, true, null
+                    ),
+                    Password(
+                        "", "Home wifi", "123456", null, true, null
+                    ),
+                    Password(
+                        "", "Home wifi", "123456", null, true, null
+                    ),
+                )
+            )
+        )
     }
 }
