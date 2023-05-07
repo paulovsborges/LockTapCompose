3909aecc0cd7c9b4d3055713bd5f2d69f54a198f
borges <paulo.borges@premiersoft.net>
2023-05-07 11:24:43 -0200

    feat: add action to delete password

--- presentation/src/main/java/com/pvsb/presentation/passwords/passwordsDetails/PasswordDetailsActivity.kt
+++ presentation/src/main/java/com/pvsb/presentation/passwords/passwordsDetails/PasswordDetailsActivity.kt
@@ -13,9 +13,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetState
+import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.Text
+import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -24,20 +29,24 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.pvsb.domain.entity.TypedMessage
 import com.pvsb.presentation.R
 import com.pvsb.presentation.ui.AppStyle.AppColors
 import com.pvsb.presentation.ui.AppStyle.TextStyles.titleTextStyle
 import com.pvsb.presentation.utils.components.BackButton
+import com.pvsb.presentation.utils.components.ComposeBottomSheetDialog
 import com.pvsb.presentation.utils.components.ComposeErrorCard
 import com.pvsb.presentation.utils.components.PrimaryButton
 import com.pvsb.presentation.utils.components.textField.PrimaryTextField
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class PasswordDetailsActivity : ComponentActivity() {
 
     private val viewModel: PasswordDetailsViewModel by viewModels()
 
+    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -52,21 +61,62 @@ class PasswordDetailsActivity : ComponentActivity() {
         }
 
         setContent {
-            val state = viewModel.state.collectAsState()
 
+            val bottomSheetState = rememberModalBottomSheetState(
+                initialValue = ModalBottomSheetValue.Hidden,
+                confirmValueChange = { true },
+                skipHalfExpanded = true
+            )
+
+            val scope = rememberCoroutineScope()
+
+            val state = viewModel.state.collectAsState()
             if (state.value.shouldCloseScreen) finish()
 
             ComposeContent(
                 state = state.value,
-                isContactDetails = passwordId != null
+                isContactDetails = passwordId != null,
+                onDeleteClick = {
+                    scope.launch {
+                        bottomSheetState.show()
+                    }
+                }
+            )
+
+            ComposeDeleteContactConfirmationDialog(
+                state = bottomSheetState,
+                onPositiveClick = {
+                    viewModel.deletePassword(state.value.passwordDetails.details.id)
+                }
             )
         }
+    }
+
+    @OptIn(ExperimentalMaterialApi::class)
+    @Composable
+    private fun ComposeDeleteContactConfirmationDialog(
+        state: ModalBottomSheetState,
+        onPositiveClick: () -> Unit
+    ) {
+
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
+            ComposeBottomSheetDialog(
+                state = state,
+                positiveBtnLabel = R.string.button_label_confirm,
+                negativeBtnLabel = R.string.button_label_cancel,
+                title = TypedMessage.Reference(R.string.bottom_sheet_confirmation_title),
+                message = TypedMessage.Reference(R.string.delete_password_dialog_message),
+                onPositiveClick = onPositiveClick
+            )
+        }
+
     }
 
     @Composable
     private fun ComposeContent(
         state: PasswordDetailsState = PasswordDetailsState(),
-        isContactDetails: Boolean = false
+        isContactDetails: Boolean = false,
+        onDeleteClick: () -> Unit = {}
     ) {
 
         Box(
@@ -105,7 +155,7 @@ class PasswordDetailsActivity : ComponentActivity() {
                             color = AppColors.red,
                             fontSize = 16.sp,
                             modifier = Modifier.clickable {
-                                viewModel.deletePassword(state.passwordDetails.details.id)
+                                onDeleteClick()
                             }
                         )
                     }
