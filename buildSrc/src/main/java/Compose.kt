c25b1f64da1afd8ebc71fb21b2e3ac95fa549fa9
pvsb <pvsbdev@gmail.com>
2025-01-31 18:50:42 -0300

    refactor: move dependencies declared on the compose and cameraX folder to toml

--- buildSrc/src/main/java/Compose.kt
+++
@@ -1,8 +0,0 @@
-object Compose {
-    val activity = "androidx.activity:activity-compose:1.7.0"
-    val material = "androidx.compose.material:material:1.4.0"
-    val constraintLayout = "androidx.constraintlayout:constraintlayout-compose:1.0.1"
-    val preview = "androidx.compose.ui:ui-tooling-preview:1.4.0"
-    val coil = "io.coil-kt:coil-compose:2.3.0"
-    val navigation = "androidx.navigation:navigation-compose:2.6.0-alpha08"
-}
