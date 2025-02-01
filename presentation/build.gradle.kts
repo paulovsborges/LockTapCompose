b8e2e979da573a4fd0753c6c76a859925d4da6dc
pvsb <pvsbdev@gmail.com>
2025-02-01 12:52:02 -0300

    feat: upgrade gradle version from 7.4 to 8.7 and Java version from 1.8 to 17

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         compose = true
@@ -38,7 +38,7 @@ android {
         kotlinCompilerExtensionVersion = "1.4.3"
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
